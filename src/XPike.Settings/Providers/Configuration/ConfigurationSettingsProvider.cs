5194f194065d8229b8e9915e974c352c065dff78
shadowxtc <chris@grifball.net>
2019-12-12 21:14:00 -0500

    Update to latest XPike.Config

--- src/XPike.Settings/Providers/Configuration/ConfigurationSettingsProvider.cs
+++ src/XPike.Settings/Providers/Configuration/ConfigurationSettingsProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using XPike.Configuration;
 
 namespace XPike.Settings.Providers.Configuration
@@ -20,16 +21,31 @@ namespace XPike.Settings.Providers.Configuration
         public string GetValue(string key) =>
             _configurationService.GetValue(key);
 
+        public Task<string> GetValueAsync(string key) =>
+            _configurationService.GetValueAsync(key);
+
         public T GetValue<T>(string key) =>
             _configurationService.GetValue<T>(key);
+
+        public Task<T> GetValueAsync<T>(string key) =>
+            _configurationService.GetValueAsync<T>(key);
 
         public string GetValueOrDefault(string key, string defaultValue = null) =>
             _configurationService.GetValueOrDefault(key, defaultValue);
 
+        public Task<string> GetValueOrDefaultAsync(string key, string defaultValue = null) =>
+            _configurationService.GetValueOrDefaultAsync(key, defaultValue);
+
         public T GetValueOrDefault<T>(string key, T defaultValue = default) =>
             _configurationService.GetValueOrDefault(key, defaultValue);
 
+        public Task<T> GetValueOrDefaultAsync<T>(string key, T defaultValue = default(T)) =>
+            _configurationService.GetValueOrDefaultAsync<T>(key, defaultValue);
+
         public IDictionary<string, string> Load() =>
             _configurationService.Load();
+
+        public Task<IDictionary<string, string>> LoadAsync() =>
+            _configurationService.LoadAsync();
     }
 }
