9ccf024ac2b2e52bb059fea2f6527a87618be89d
shadowxtc <chris@grifball.net>
2019-12-14 04:15:09 -0500

    Add AddSettingsPipe extension method

---
+++ src/XPike.Settings/IDependencyProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using XPike.Configuration.Pipeline;
+using XPike.IoC;
+
+namespace XPike.Settings
+{
+    public static class IDependencyProviderExtensions
+    {
+        public static IDependencyProvider AddSettingsPipe<TPipe>(this IDependencyProvider provider)
+            where TPipe : class, IConfigurationPipe
+        {
+            provider.ResolveDependency<ISettingsService>()
+                .AddToPipeline(provider.ResolveDependency<TPipe>());
+
+            return provider;
+        }
+    }
+}
